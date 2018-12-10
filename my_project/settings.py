5cf98564164756b6e2aa34d39c9a24c3d49260be
darklordace <firdaus.alghifari@gmail.com>
2018-12-10 18:43:48 +0700

    4th Commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +122,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATCFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATCFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
